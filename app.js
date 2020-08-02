800e873cd0705d2742a2bef5a77f99c949c99330
Facundo Jaton <facundoe.jaton@gmail.com>
2020-08-02 20:20:58 -0300

    End of chapter 5

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
-Vue.filter("Mayus", function (stringToUppcase) {
-    return stringToUppcase.toUpperCase();
+Vue.component('mi-primer-componente', {
+    props: ['message'],
+    methods: {
+        sayHi: function(value){
+            alert("Hola " +value);
+        }
+    },
+    template: '<div class="alert alert-primary" role="alert"><button class="btn btn-secondary" v-on:click="sayHi(message)">El botón</button>{{message}}</div>'
 })
-
 var app = new Vue({
     el: "#app",
     data: {
-        votes: 0,
-        name: "",
-        lastName: "0"
-    },
-    methods: {
-        vote: function () {
-            this.votes++;
-            console.log(this.votes)
-        }
-    },
 
-    computed: {
-        completeName: function () {
-            return this.name + " " + this.lastName
-        }
     }
 })
